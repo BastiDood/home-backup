11a5002bcf13ff0e186d13e010a4522741a3e3b8
Basti Ortiz <ortizbasti16@gmail.com>
2018-12-19 17:51:55 +0800

    Added description for parameters

--- src/middlewares/logRequests.js
+++ src/middlewares/logRequests.js
@@ -2,9 +2,9 @@
 
 /**
  * Logs the IP address and the requested resouce of the incoming request.
- * @param {Request} req
- * @param {Response} res
- * @param {Function} next
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @param {Function} next -  The next middleware function to be called by Express.
  */
 function logRequests(req, res, next) {
   console.log(`Request from: ${req.ip}`);
