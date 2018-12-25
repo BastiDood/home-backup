ae6951340da13244713f42167a24f3932e79fab8
Basti Ortiz <ortizbasti16@gmail.com>
2018-12-25 15:22:50 +0800

    Refactored router to enable interception of all HTTP verbs

--- src/routes/fileHandler.js
+++ src/routes/fileHandler.js
@@ -7,17 +7,18 @@ const router = require('express').Router();
 // USER IMPORTS
 const getUploadsDirectory = require('../util/getUploadsDirectory');
 
-router.get('*', (req, res) => {
-  const filepath = req.params[0];
-
-  getUploadsDirectory(filepath)
-    .then(files => {
-      res.render('files', { filepath, files });
-    })
-    .catch(error => {
-      res.statusMessage = 'File or directory not found';
-      res.status(404).render('files', { filepath, error } );
-    });
-});
+// Intercepts all routes from /files/*
+router.route('*')
+  .get((req, res) => {
+    const filepath = req.params[0];
+    getUploadsDirectory(filepath)
+      .then(files => {
+        res.render('files', { filepath, files });
+      })
+      .catch(error => {
+        res.statusMessage = 'File or directory not found';
+        res.status(404).render('files', { filepath, error } );
+      });
+  });
 
 module.exports = router;
