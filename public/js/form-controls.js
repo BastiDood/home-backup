926fb6549bccd47a45b689a9e6e80294e3e874b0
Basti Ortiz <ortizbasti16@gmail.com>
2018-12-28 20:27:53 +0800

    Added input text field on click of 'New Folder' Button

--- public/js/form-controls.js
+++ public/js/form-controls.js
@@ -3,8 +3,21 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('fsControl');
   const filesUpload = document.getElementById('filesUpload');
+  const newFolderButton = document.getElementById('newFolder');
 
+  // Submit the file as soon as the user selects
   filesUpload.addEventListener('change', function() {
     if (!this.length) form.submit();
+  });
+
+  // Create a new folder
+  newFolderButton.addEventListener('click', () => {
+    const table = document.getElementsByClassName('table')[0];
+    const beforeTarget = document.getElementsByClassName('before-target')[0];
+    
+    const input = document.createElement('input');
+    input.type = 'text';
+
+    table.insertBefore(input, beforeTarget);
   });
 });
