2fe008f98fdfd6613188c9f5a5d936dc2eca1c41
Basti Ortiz <ortizbasti16@gmail.com>
2018-12-28 22:49:57 +0800

    Added documentation for `createCell` module

--- public/js/create-cell.js
+++ public/js/create-cell.js
@@ -1,3 +1,11 @@
+/**
+ * Creates a single cell of data.
+ * @param {string} [text=''] - Text to be inserted as a text node
+ * @param {string} type - CSS class to apply (either `text`
+ * or `number` only)
+ * @param {boolean} [hasTextInput=false] - Whether or not to include an <input> tag inside the resulting cell
+ * @returns {HTMLDivElement} The resulting cell element
+ */
 function createCell(text = '', type, hasTextInput = false) {
   const textNode = document.createTextNode(text);
   const cell = document.createElement('div');
